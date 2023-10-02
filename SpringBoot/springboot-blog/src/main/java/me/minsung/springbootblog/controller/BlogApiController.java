b10dd611f5e005f2bcfcd1132a9f096aa366e83f
강민성 <eclipse65@gmail.com>
2023-10-02 16:27:33 +0900

    블로그 글 목록 조회 API와 테스트 작성

--- SpringBoot/springboot-blog/src/main/java/me/minsung/springbootblog/controller/BlogApiController.java
+++ SpringBoot/springboot-blog/src/main/java/me/minsung/springbootblog/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.minsung.springbootblog.controller;
 import lombok.RequiredArgsConstructor;
 import me.minsung.springbootblog.domain.Article;
 import me.minsung.springbootblog.dto.AddArticleRequest;
+import me.minsung.springbootblog.dto.ArticleResponse;
 import me.minsung.springbootblog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +24,16 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
---
+++ SpringBoot/springboot-blog/src/main/java/me/minsung/springbootblog/dto/ArticleResponse.java
@@ -0,0 +1,15 @@
+package me.minsung.springbootblog.dto;
+
+import lombok.Getter;
+import me.minsung.springbootblog.domain.Article;
+
+@Getter
+public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
