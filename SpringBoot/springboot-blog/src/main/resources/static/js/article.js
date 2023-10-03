775eb6146a3aa0df05e32cfc0f3a784580324731
강민성 <eclipse65@gmail.com>
2023-10-03 15:29:54 +0900

    글 수정 구현

--- SpringBoot/springboot-blog/src/main/resources/static/js/article.js
+++ SpringBoot/springboot-blog/src/main/resources/static/js/article.js
@@ -1,3 +1,4 @@
+// 삭제 기능
 const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton) {
@@ -9,6 +10,31 @@ if (deleteButton) {
         .then(() => {
             alert('삭제가 완료되었습니다.');
             location.replace('/articles')
+        });
+    });
+}
+
+// 수정 기능
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+        .then(() => {
+            alert('수정이 완료되었습니다.');
+            location.replace(`articles/${id}`);
         });
     });
 }
