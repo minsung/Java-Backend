f6afc3b4a22a144a097d55101b7a6f0a745e19f7
강민성 <eclipse65@gmail.com>
2023-10-03 13:43:39 +0900

    블로그 글 목록 뷰 구현

---
+++ SpringBoot/springboot-blog/src/main/java/me/minsung/springbootblog/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package me.minsung.springbootblog.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.minsung.springbootblog.dto.ArticleListViewResponse;
+import me.minsung.springbootblog.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
