4fbb3cf4bb5228db6a948ae81acd3f01f39ace8c
강민성 <eclipse65@gmail.com>
2023-10-07 13:37:41 +0900

    스프링 시큐리티로 로그인 / 로그아웃, 회원 가입 구현

---
+++ SpringBoot/springboot-blog/src/main/java/me/minsung/springbootblog/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.minsung.springbootblog.repository;
+
+import me.minsung.springbootblog.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ SpringBoot/springboot-blog/src/main/java/me/minsung/springbootblog/service/UserService.java
@@ -0,0 +1,23 @@
+package me.minsung.springbootblog.service;
+
+import lombok.RequiredArgsConstructor;
+import me.minsung.springbootblog.domain.User;
+import me.minsung.springbootblog.dto.AddUserRequest;
+import me.minsung.springbootblog.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
